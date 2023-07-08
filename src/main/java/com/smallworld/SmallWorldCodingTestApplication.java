70e8d806f4210d46873d53f4845fc14a03e23fc8
Ahsan Aazmi <ahsan.raza@medznmore.com>
2023-07-08 16:51:05 +0500

    Add initial project files to Git

---
+++ src/main/java/com/smallworld/SmallWorldCodingTestApplication.java
@@ -0,0 +1,13 @@
+package com.smallworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallWorldCodingTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallWorldCodingTestApplication.class, args);
+	}
+
+}
